779a48acbc1c7fbbad23beddcbbc7ee4b10c8095
Riccardo Manzoni <riccardo.manzoni@cern.ch>
2016-11-16 16:03:16 +0100

    start doodling down some FPix too

---
+++ utils/DQMDisk.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import ROOT
+import sys
+sys.path.append('..')
+from itertools import product
+import numpy as np
+
+def binx(module, roc):
+    '''
+    IMPLEMENT ME!
+    '''
+    pass
+
+def biny(ladder, roc, layer):
+    '''
+    IMPLEMENT ME!
+    '''
+    pass
+
+def readOccupancyPlot(occupancy):
+    '''
+    IMPLEMENT ME!
+    '''
+    pass
+
+def fillOccupancyPlot(rocs, layer):
+    '''
+    IMPLEMENT ME!
+    '''
+    pass
+    
+if __name__ == '__main__':
+    pass    
